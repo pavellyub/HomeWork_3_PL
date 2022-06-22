63eef27acde187928f11941a92810b994d30d08c
pavellyub <107138984+pavellyub@users.noreply.github.com>
2022-06-22 19:47:01 +0500

    Second Task

---
+++ Task_2.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int xa = Input("Введите координату Х точки А: ");
+int ya = Input("Введите координату Y точки А: ");
+int za = Input("Введите координату Z точки А: ");
+int xb = Input("Введите координату X точки B: ");
+int yb = Input("Введите координату Y точки B: ");
+int zb = Input("Введите координату Z точки B: ");
+
+int lenghtX = (xa - xb);
+int lenghtY = (ya - yb);
+int lenghtZ = (za - zb);
+
+double result = (Quadro(lenghtX) + Quadro(lenghtY) + Quadro(lenghtZ));
+double Cube = Math.Sqrt(result);
+Console.WriteLine($"Расстояние между точками составляет: {Cube}");
+
+int Quadro (int a)
+{
+    return a * a;
+}
+
+int Input (string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
