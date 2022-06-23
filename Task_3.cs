a0acdc1c89f3620dac7ded2f33ff78c98e739779
pavellyub <107138984+pavellyub@users.noreply.github.com>
2022-06-23 16:09:52 +0500

    Third task

---
+++ Task_3.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int N = Input("Введите натурльаное число: ");
+
+int count = 0;
+int result = 0;
+
+while (count < N)
+{
+    result = Cube(count);
+    count = count + 1;
+    Console.Write($"{result}, ");
+}
+
+Console.WriteLine(Cube(N));
+
+int Cube(int a)
+{
+    return a * a * a;
+}
+
+int Input (string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
